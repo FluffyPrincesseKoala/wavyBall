d77c4e65c5eeedf66e758ee996e4fe57d5caa43d
unknown <cyrillemaire1@gmail.com>
2024-03-10 14:40:52 +0100

    incrase displacement calculation in vertex shaders

--- glsl/plane/vert.glsl
+++ glsl/plane/vert.glsl
@@ -9,7 +9,7 @@ uniform float u_frequency;
 
 void main() {
     float noise = 3.0 * pnoise(position + u_time, vec3(10.0));
-    float displacement = (u_frequency / 40.) * (noise / 10.);
+    float displacement = (u_frequency / 30.) * (noise / 10.);
     vec3 newPosition = position + normal * displacement;
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 }
