1b3b5ad3ca10d431f68d7f511f6c6ca4692c3826
unknown <cyrillemaire1@gmail.com>
2024-07-03 19:56:19 +0200

    fix: refactor fragment shader for improved color calculation

--- glsl/plane/frag.glsl
+++ glsl/plane/frag.glsl
@@ -11,10 +11,15 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  vec3 color = vec3(0.0);
+  // vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / u_resolution.y;
+  vec2 uv = (gl_FragCoord.xy) / u_resolution.xy;
+  vec3 color = vec3(0.0, 0.0, 0.0);
   float d = distance(uv, vec2(0.5));
-  float freq = u_frequency > 0. ? normalize(u_frequency) : 1.0;
-  color = vec3(sin(u_time * freq + d * 10.0 + u_red), cos(u_time * freq + d * 10.0 + u_green), fract(u_time * freq + d * 10.0 + u_blue)) * 0.5 + 0.5;
+  float freq = u_frequency > 0. ? normalize(u_frequency) : 0.5;
+  float time = u_time * .2;
+  float red = sin(abs(time * freq + d + u_red));
+  float green = (cos(abs(time * freq + d + u_green)));
+  float blue = (sin(abs(time * freq + d + u_blue)));
+  color = vec3(red, green, blue) * 0.5 + 0.5;
   gl_FragColor = vec4(color, 1.0);
 }
